09614d856292d733f9d2b981464e32a82647939f
Bohdan Miskov <bohdanmiskov07@gmail.com>
2026-01-13 14:33:42 +0200

    Created init script for table, bucked and lambda

--- lib/smart-receipts-backend-stack.ts
+++ lib/smart-receipts-backend-stack.ts
@@ -1,16 +1,64 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
 
 export class SmartReceiptsBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DYNAMODB
+    const table = new dynamodb.Table(this, 'ReceiptTable', {
+      partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SmartReceiptsBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // S3 BUCKET
+    const bucket = new s3.Bucket(this, 'ReceiptBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+
+      cors: [
+        {
+          allowedMethods: [
+            s3.HttpMethods.GET,
+            s3.HttpMethods.PUT,
+            s3.HttpMethods.HEAD,
+          ],
+          allowedOrigins: ['*'],
+          allowedHeaders: ['*'],
+        },
+      ],
+    });
+
+    // LAMBDA FUNCTION
+    const processor = new nodejs.NodejsFunction(this, 'ReceiptProcessor', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'handler',
+      entry: path.join(__dirname, '../src/lambda/processor.ts'),
+      environment: {
+        TABLE_NAME: table.tableName,
+        BUCKET_NAME: bucket.bucketName,
+      },
+      timeout: cdk.Duration.seconds(30),
+    });
+
+    // PERMISSIONS
+    bucket.grantRead(processor);
+    table.grantWriteData(processor);
+
+    // OUTPUTS
+    new cdk.CfnOutput(this, 'TableName', {
+      value: table.tableName,
+      description: 'The name of the DynamoDB table',
+    });
+    new cdk.CfnOutput(this, 'BucketName', { value: bucket.bucketName });
+    new cdk.CfnOutput(this, 'LambdaName', { value: processor.functionName });
   }
 }
