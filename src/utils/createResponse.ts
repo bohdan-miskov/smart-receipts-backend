043aaa59e14479a740d4ab1744e979dc7babb92e
Bohdan Miskov <bohdanmiskov07@gmail.com>
2026-03-02 18:51:53 +0200

    Feature/coors fix (#4)
    * Added default headers for response
    
    * Updated oidc

--- src/utils/createResponse.ts
+++ src/utils/createResponse.ts
@@ -1,16 +1,24 @@
+const DEFAULT_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Content-Type': 'application/json',
+};
+
 export const createResponse = (
   code: number,
   message?: string,
   data?: object,
-  headers?: Record<string, string>,
+  headers?: Record<string, string | boolean>,
 ) => {
   const body = {
     message,
     data,
   };
+
   return {
     statusCode: code,
-    headers,
+    headers: headers ?? DEFAULT_HEADERS,
     body: JSON.stringify(body),
   };
 };
